4b196090224cc4577ebe73b7030b751cb4f95331
Nelson2023-2024 <nelsonobuya19@gmail.com>
2024-09-11 21:29:52 +0300

    Send verification email

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,8 +1,8 @@
-import {VERIFICATION_EMAIL_TEMPLATE} from './emailtemplates.js';
-import {mailtrapClient, recipients, sender} from './mailtrap.config.js';
+import { VERIFICATION_EMAIL_TEMPLATE } from './emailtemplates.js'
+import { mailtrapClient, recipients, sender } from './mailtrap.config.js'
 
 export const sendVerificationEmail = async (email, verificationToken) => {
-  const recipient = [{email}];
+  const recipient = [{ email }]
   try {
     const response = await mailtrapClient.send({
       from: sender,
@@ -12,12 +12,31 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         '{verificationCode}',
         verificationToken
       )}`,
-      category: 'Email verification',
-    });
+      category: 'Email verification'
+    })
 
-    console.log(`Email sent succefull`);
+    console.log(`Email sent succefull`)
   } catch (error) {
-    console.log(`Error sending email` + error.message);
-    throw new Error(`Error sending verification email: ${email}`);
+    console.log(`Error sending email` + error.message)
+    throw new Error(`Error sending verification email: ${email}`)
   }
-};
+}
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }]
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: '908f8258-913e-41d0-9fe0-520b64e60236',
+      template_variables: {
+        company_info_name: 'Auth Company',
+        name: `${name}`
+      }
+    })
+    console.log('Welcome Email sent succefully', response)
+  } catch (error) {
+    console.error('Error sending the welcome email', error)
+    throw new Error(`Error sending welcome email: ${error}`)
+  }
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,66 +1,103 @@
-import bcryptjs from 'bcryptjs';
+import bcryptjs from 'bcryptjs'
 
-import {Router} from 'express';
-import {login, logout, signUp} from '../controllers/auth.controller.js';
-import {User} from '../models/user.model.js';
-import {generateTokenAndSetCookie} from '../utils/generateTokenAndSetCookie.js';
-import {sendVerificationEmail} from '../mailtrap/emails.js';
+import { Router } from 'express'
+import { login, logout, signUp } from '../controllers/auth.controller.js'
+import { User } from '../models/user.model.js'
+import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js'
+import { sendVerificationEmail, sendWelcomeEmail } from '../mailtrap/emails.js'
 
-const router = Router();
+const router = Router()
 
 router.post('/signup', async (req, res) => {
-  const {email, password, name} = req.body;
+  const { email, password, name } = req.body
 
   try {
-    if (!email || !password || !name)
-      throw new Error('All inputs are required');
+    if (!email || !password || !name) throw new Error('All inputs are required')
 
-    const userAlreadyExists = await User.findOne({email});
-    console.log(userAlreadyExists);
+    const userAlreadyExists = await User.findOne({ email })
+    console.log(userAlreadyExists)
     if (userAlreadyExists)
-      return res.status(400).json({message: 'User already exists'});
+      return res.status(400).json({ message: 'User already exists' })
 
-    const hashedPassword = await bcryptjs.hash(password, 10);
+    const hashedPassword = await bcryptjs.hash(password, 10)
 
     //creating a verification code
     const verificationToken = Math.floor(
       100000 + Math.random() * 900000
-    ).toString();
+    ).toString()
 
     const user = new User({
       email,
       password: hashedPassword,
       name,
       verificationToken,
-      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
-    });
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000
+    })
 
-    await user.save();
+    await user.save()
 
     //jwt
-    generateTokenAndSetCookie(res, user._id);
+    generateTokenAndSetCookie(res, user._id)
 
     //sending verification email
-    await sendVerificationEmail(user.email, verificationToken);
+    await sendVerificationEmail(user.email, verificationToken)
 
     res.status(201).json({
       msg: 'User created succefully',
       user: {
         ...user._doc, //retun the whole user document
-        password: undefined, //keep the password field undefined
-      },
-    });
+        password: undefined //keep the password field undefined
+      }
+    })
   } catch (error) {
-    return res.status(400).json({success: false, message: error.message});
+    return res.status(400).json({ success: false, message: error.message })
   }
-});
+})
+
+router.post('/verify-email', async (req, res) => {
+  //1 2 3 4 5 6
+  const { code } = req.body
+  try {
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() }
+    })
+
+    //
+    if (!user)
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid or expired verification code'
+      })
+    //if the token is found and not expired we are not going to be needing them any more
+    user.isVerified = true
+    user.verificationToken = undefined
+    user.verificationTokenExpiresAt = undefined
+    await user.save()
+
+    //sending welcome email
+    await sendWelcomeEmail(user.email, user.name)
+
+    res.status(200).json({
+      success: true,
+      message: 'Email verified successfully',
+      user: {
+        ...user._doc,
+        password: undefined
+      }
+    })
+  } catch (error) {
+    console.log('error in verifyEmail', error)
+    res.status(500).json({ success: false, message: 'Sever Error' })
+  }
+})
 
 router.post('/login', (req, res) => {
-  res.send('login route', login);
-});
+  res.send('login route')
+})
 
 router.post('/logout', (req, res) => {
-  res.send('logout route', logout);
-});
+  res.send('logout route')
+})
 
-export default router;
+export default router
